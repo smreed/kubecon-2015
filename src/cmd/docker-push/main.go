8c86edc8a96066b2dad0eb0e0fc317348608be39
Steve Reed <steven.m.reed@gmail.com>
2015-11-04 10:57:10 -0800

    Builds multiple images concurrently

--- src/cmd/docker-push/main.go
+++ src/cmd/docker-push/main.go
@@ -2,27 +2,48 @@ package main
 
 import (
 	"log"
+	"sync"
 
 	"github.com/smreed/kubecon-2015/pkg/util"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	images, err := util.FindImagesInCwd()
 	if err != nil {
 		log.Fatal("error finding images:", err)
 	}
-	for _, img := range images {
-		log.Println("Building", img)
-		if err = img.Build(); err != nil {
-			log.Println("error building", img)
-			log.Fatal(err)
-		}
-		log.Println("Pushing", img)
-		if err = img.Push(); err != nil {
-			log.Println("error ppushing", img)
-			log.Fatal(err)
-		}
-	}
 
+	wg.Add(len(images))
+	built := make(chan util.Image)
+
+	// Build images in goroutines
+	go func() {
+		for _, img := range images {
+			go func(img util.Image) {
+				log.Println("Building", img)
+				if err = img.Build(); err != nil {
+					log.Println("error building", img)
+					log.Fatal(err)
+				}
+				built <- img
+			}(img)
+		}
+	}()
+
+	// Push built images in goroutines
+	go func() {
+		for img := range built {
+			log.Println("Pushing", img)
+			if err = img.Push(); err != nil {
+				log.Println("error ppushing", img)
+				log.Fatal(err)
+			}
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
 	log.Println("Done")
 }
